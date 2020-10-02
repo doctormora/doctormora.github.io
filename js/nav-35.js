9e077380cf3bef2a0c198a3adcd44080572acd18
cesar pablo gs <ces.studio.arp@gmail.com>
2020-10-01 19:14:51 -0500

    menu top

---
+++ js/nav-35.js
@@ -0,0 +1,39 @@
+(function( window ){
+
+    var body = document.body,
+        mask = document.createElement("div"),
+        
+        toggleSlideRight = document.querySelector( ".toggle-slide-right" ),
+        
+        activeNav
+    ;
+    mask.className = "mask";
+
+ 
+
+    /* slide menu right */
+    toggleSlideRight.addEventListener( "click", function(){
+        classie.add( body, "smr-open" );
+        document.body.appendChild(mask);
+        activeNav = "smr-open";
+    } );
+
+ 
+
+    /* hide active menu if mask is clicked */
+    mask.addEventListener( "click", function(){
+        classie.remove( body, activeNav );
+        activeNav = "";
+        document.body.removeChild(mask);
+    } );
+
+    /* hide active menu if close menu button is clicked */
+    [].slice.call(document.querySelectorAll(".close-menu")).forEach(function(el,i){
+        el.addEventListener( "click", function(){
+            classie.remove( body, activeNav );
+            activeNav = "";
+            document.body.removeChild(mask);
+        } );
+    });
+
+})( window );
---
+++ js/nav.js
@@ -0,0 +1,7 @@
+	$('#mostrar').on('click',function(){
+			$('#men').toggleClass('menu-movil-m');			
+	 })
+	
+    $('#flec-arr').on('click',function(){
+		$('#men').removeClass('menu-movil-m');
+	})
