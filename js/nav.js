15f610e9545852c1eafa739a8aa1cb00d27a7ea8
cesar pablo gs <ces.studio.arp@gmail.com>
2020-10-18 01:11:37 -0500

    se corrigio el error en el menu hover

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 	$('#mostrar').on('click',function(){
 			$('#men').toggleClass('menu-movil-m');
-			$('body').css({overflow: "hidden"});
+			$('body').toggleClass('ocul'});
 	 })
 	
     $('#cerrar-men').on('click',function(){
