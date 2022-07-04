4b659edc25475fed640a0f8039a862b9165d0ad5
cesarpgs <cesarpgasuz@gmail.com>
2022-07-04 15:35:38 -0500

    version html
    se cambiara a html nuevamente, para usar react en adelante

--- eventos.php
+++ eventos.php
@@ -1,4 +1,111 @@
-<?php include_once 'includes/templates/header.php'; ?>
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
+	<title>Doctor Mora</title>
+	<link rel="shortcut icon" href="img/icon.ico" />
+	<script src="js/pace.min.js"></script> 
+	  
+	  
+	
+	  
+	<!-- fuentes  -->
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;900&display=swap" rel="stylesheet">
+	
+	<!-- fuentes iconmoon -->
+	<link rel="stylesheet" href="css/fonts.css">
+	<link rel="stylesheet" href="fonts/coronavirus/style.css">
+	  
+	<!-- font awesome -->  
+	<script src="https://kit.fontawesome.com/871e8e7b15.js" crossorigin="anonymous"></script>
+	  
+
+	<!-- estilos carusel -->
+	<link rel="stylesheet" href="owl/owl.carousel.css">
+	<link rel="stylesheet" href="owl/owl.theme.default.css"> 
+	  
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	
+	<!-- estilos css -->  
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/styles2.css"> <!-- page coronavirus -->
+	
+
+	<!-- PWA-->
+	<meta name="theme-color" content="#f0f0f0"> 
+	<link rel="manifest" href="manifest.json">
+	  
+
+	  <!-- onesignal -->
+	  <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+		<script>
+		  window.OneSignal = window.OneSignal || [];
+		  OneSignal.push(function() {
+			OneSignal.init({
+			  appId: "075d924a-c91c-4506-9ddb-e628eb044779",
+			});
+		  });
+		</script>
+
+	  
+	
+  </head>
+  <body>
+	
+	  <div class="sombra">
+
+		</div>
+	
+	  
+	<!-- menu fijo movil solo se activa en dispositivos moviles -->
+	<section class="list-top fixed-top">
+		<div class="container d-flex align-items-center">
+			 
+				<svg width="1.8em" height="1.8em" viewBox="0 0 16 16" id="mostrar" class="bi bi-justify" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="z-index: 3;">
+	  <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+	</svg>
+
+			<span>Doctor Mora</span>
+			
+			
+			
+		    <!-- <div class="spinner-border spinner-border-sm" id="carga" role="status"><span class="sr-only">Loading...</span></div> -->
+		  	<div class="ml-auto d-flex" style="z-index: 3;">
+		  	<div class="onesignal-customlink-container"></div>
+			<a href="coronavirus.php"><img src="img/bacterias.svg" class="img-fluid" width="30px;" style="margin-left: 20px;"></a>
+			</div>
+	 	</div>
+	</section> 
+	<!-- fin seccion menu -->
+	  
+	 <!-- menu version escritorio --> 
+	
+	  <nav class="navbar navbar-expand-lg menu-escritorio fixed-top">
+		  <div class="container">
+		  
+			   <a class="navbar-brand" href="index.php">Doctor Mora</a>
+				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				  </button>
+			  
+				  <div class="enlaces-m-escritorio" id="navbarNavAltMarkup">
+					<div class="ml-auto">
+					  <a href="index.php">Inicio</a>
+					  <a href="#enlaces">Links útiles</a>
+					  <a href="#contacto">Contacto</a>
+					  <a href="coronavirus.php">COVID-19</a>
+					  <a href="#datos">Bugs <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bug-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.978.855a.5.5 0 1 0-.956.29l.41 1.352A4.985 4.985 0 0 0 3 6h10a4.985 4.985 0 0 0-1.432-3.503l.41-1.352a.5.5 0 1 0-.956-.29l-.291.956A4.978 4.978 0 0 0 8 1a4.979 4.979 0 0 0-2.731.811l-.29-.956zM13 6v1H8.5v8.975A5 5 0 0 0 13 11h.5a.5.5 0 0 1 .5.5v.5a.5.5 0 1 0 1 0v-.5a1.5 1.5 0 0 0-1.5-1.5H13V9h1.5a.5.5 0 0 0 0-1H13V7h.5A1.5 1.5 0 0 0 15 5.5V5a.5.5 0 0 0-1 0v.5a.5.5 0 0 1-.5.5H13zm-5.5 9.975V7H3V6h-.5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 0-1 0v.5A1.5 1.5 0 0 0 2.5 7H3v1H1.5a.5.5 0 0 0 0 1H3v1h-.5A1.5 1.5 0 0 0 1 11.5v.5a.5.5 0 1 0 1 0v-.5a.5.5 0 0 1 .5-.5H3a5 5 0 0 0 4.5 4.975z"/>
+</svg> </a>
+					</div>
+				  </div>
+		  </div>
+	</nav>
+	<!-- fin menu version escritorio -->  
+	  
 	  
 	  
 	  
@@ -160,4 +267,222 @@
 
 
 
-<?php include_once 'includes/templates/footer.php'; ?>
+
+
+<section class="dr-mora">
+	  	<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<h6>Desarrollado con <img src="img/corazon.png" class="img-fluid corazon" alt="" width="25px"> en Doctor Mora Gto. </h6>
+				</div>
+			</div>
+		</div>
+	  </section>
+
+
+	
+
+
+  
+	<!-- menu flotante (solo se activa en dispositivos moviles) -->  
+	<div id="men" class="menu-movil">
+		<div class="men-cont">
+			<span class="cerrar">
+				<svg width="3.5em" height="3.5em" viewBox="0 0 16 16" id="cerrar-men" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+		  			<path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+				</svg>
+			</span>
+			<div class="men-cont-c">
+				<ul class="list-unstyled men-cont-links">
+					<li><a href="faqs.php"><i class="fas fa-question"></i> FAQs</a></li>
+					<li><a href="contacto.php"><i class="fas fa-child"></i> Acerca de Nosotros</a></li>
+					<li><a href="contacto.php"><i class="fas fa-code"></i> Acerca de la plataforma</a></li>
+					<li><a href="contacto.php"><i class="far fa-angry"></i> Quejas, sugerencias, etc...</a></li>
+				</ul>
+			</div>
+			<div class="men-cont-c">
+				<a href="#" class="bug"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bug-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.978.855a.5.5 0 1 0-.956.29l.41 1.352A4.985 4.985 0 0 0 3 6h10a4.985 4.985 0 0 0-1.432-3.503l.41-1.352a.5.5 0 1 0-.956-.29l-.291.956A4.978 4.978 0 0 0 8 1a4.979 4.979 0 0 0-2.731.811l-.29-.956zM13 6v1H8.5v8.975A5 5 0 0 0 13 11h.5a.5.5 0 0 1 .5.5v.5a.5.5 0 1 0 1 0v-.5a1.5 1.5 0 0 0-1.5-1.5H13V9h1.5a.5.5 0 0 0 0-1H13V7h.5A1.5 1.5 0 0 0 15 5.5V5a.5.5 0 0 0-1 0v.5a.5.5 0 0 1-.5.5H13zm-5.5 9.975V7H3V6h-.5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 0-1 0v.5A1.5 1.5 0 0 0 2.5 7H3v1H1.5a.5.5 0 0 0 0 1H3v1h-.5A1.5 1.5 0 0 0 1 11.5v.5a.5.5 0 1 0 1 0v-.5a.5.5 0 0 1 .5-.5H3a5 5 0 0 0 4.5 4.975z"/>
+</svg> Reportar Bug </a>
+				<div class="dr-mora-movil">
+					<h6>Desarrollado con <img src="img/corazon.png" class="img-fluid corazon" alt="" width="20px"> en <strong>Doctor Mora Gto.</strong></h6>
+				</div>
+				
+			</div>
+			
+			
+
+
+		</div>
+	</div>
+	<!-- menu flotante (solo se activa en dispositivos moviles) -->  
+
+
+
+<!-- modal -->
+	<?php include_once 'includes/templates/modal.php'; ?>
+	
+
+<!-- fin modal -->
+
+
+
+
+
+
+
+	  
+	  <footer class="pie">
+	  	<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-4 agi">
+				
+					<img src="img/icon256n.png" class="img-fluid" alt="">
+				
+				</div>
+				<div class="col-lg-4 col-md-4 enlaces" id="enlaces">
+					<h4>Links útiles</h4>
+					<ul class="list-unstyled">
+						<li><a href="faqs.php"><i class="fas fa-question"></i> FAQS</a></li>
+						<li><a href="faqs.php#nosotros"><i class="fas fa-child"></i> Acerca de Nosotros</a></li>
+						<li><a href="faqs.php#plataforma"><i class="fas fa-code"></i> Acerca de la plataforma </a></li>
+						<li><a href="faqs.php#quejas"><i class="far fa-angry"></i> Quejas, Sugerencias, etc...</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-4 col-md-4 contacto" id="contacto">
+					<h4>Contacto</h4>
+					<ul class="list-unstyled">
+						<li><a href="#"><i class="fab fa-whatsapp"></i> 419 138 9032</a></li>
+						<li><a href="#"><i class="far fa-envelope"></i> dyxerteam@gmail.com</a></li>
+						<li><a href="apk.php"><i class="fab fa-android"></i> Descarga el APK</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-12 col-md-12 copy">
+					<p>© Charkas 2020 Todos los derechos reservados. </p>
+				</div>
+			</div>  
+		</div>
+	  </footer>
+	  
+	  
+		
+
+	  
+	  <!-- menu bottom -->
+	  <section class="menu-bottom fixed-bottom">
+	  	<div class="container">
+			<div class="row">
+				<ul class="list-unstyled list-inline">
+					<li class="list-inline-item"><a href="index.php"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
+  <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+</svg></a></li>
+					<li class="list-inline-item"><a href="eventos.php">
+						<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calendar-event" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
+  <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+</svg>
+						</a></li>
+					
+					<li class="list-inline-item"><a href="#">
+						<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-shop-window" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h12V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zm2 .5a.5.5 0 0 1 .5.5V13h8V9.5a.5.5 0 0 1 1 0V13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a.5.5 0 0 1 .5-.5z"/>
+</svg>
+						</a></li>
+					
+					
+					
+					<li class="list-inline-item"><a href="#">
+						
+						<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-people" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1h7.956a.274.274 0 0 0 .014-.002l.008-.002c-.002-.264-.167-1.03-.76-1.72C13.688 10.629 12.718 10 11 10c-1.717 0-2.687.63-3.24 1.276-.593.69-.759 1.457-.76 1.72a1.05 1.05 0 0 0 .022.004zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10c-1.668.02-2.615.64-3.16 1.276C1.163 11.97 1 12.739 1 13h3c0-1.045.323-2.086.92-3zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
+</svg>
+						
+						
+						</a></li>
+					<li class="list-inline-item">
+						<a href="#exampleModal" data-toggle="modal">
+					<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-box-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
+  <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z"/>
+</svg>
+						</a>
+						
+						</li>
+				</ul>
+			</div>  
+		</div>
+	  </section>
+	  
+	  
+    <!-- Optional JavaScript -->
+	
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	  
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="owl/owl.carousel.js"></script>
+	<script src="js/app.js"></script>
+	<script src="js/nav.js"></script>
+	<script>
+		$('.loop').owlCarousel({
+    		center: true,
+    		items:2,
+		  	dots:false,
+		  	nav: true,
+    		loop:true,
+    		autoWidth:true,
+    		margin:30,
+    		responsive:{
+        		600:{
+            		items:4
+        		}
+    		}
+		});
+		
+		$('.nonloop').owlCarousel({
+    		center: true,
+    		items:2,
+			dots:false,
+		  	nav: true,
+	 		autoWidth:true,
+    		loop:false,
+    		margin:25,
+    		responsive:{
+        		600:{
+            		items:4
+        		}
+    		}
+		});
+
+		/*
+		$(function(){
+			var barraaltura = $('.list-top').innerHeight();
+			console.log(barraaltura);
+		});
+		*/
+
+	  </script>
+
+<script>
+      $(document).ready(function () {
+        $(window).scroll(function () {
+          var scroll = $(window).scrollTop();
+          if (scroll < 100) {
+            $(".ch").css({ display: "block" });
+            $(".ev").css({ "display": "none" });
+			 
+            
+          } else {
+            $(".ch").css({ display: "none" });
+            $(".ev").css({ display: "block" });
+			 
+            
+          }
+        });
+      });
+    </script>
+					
+  </body>
+</html>
